a4dbee32774974f4e495b47dbc6481aac77dbec6
Arthur Mneney <amneney5@gmail.com>
2022-02-19 15:24:38 +0000

    user management ammended

--- username.sh
+++ username.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # This script adds username and creates userID
-echo "Please, enter your First and Last name"
+echo -n "Please, enter your First and Last name: "
 read name
-echo "Please, enter your preferred username"
+echo "Thank you very much $name"
+echo -n "Please, enter your preferred username: "
 read username
+echo "Great! $name, you have entered $username as your username"
 sudo useradd -c "$name" $username
-echo "Please, enter your secret password"
+echo "Wonderful! your username was successful created"
+echo -n "Please, enter your secret password: "
 read -s password
 sudo passwd $username
-tail -2 /etc/passwd
+echo "Below is your /etc/passwd file. Now, you can access your account"
+tail -1 /etc/passwd
